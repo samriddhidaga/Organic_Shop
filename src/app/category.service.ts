abc3c84bd565c81625ed80fdfa84b62d7c7521e0
samriddhidaga <samriddhidaga9@gmail.com>
2020-02-15 14:55:14 +0530

    minor changes

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Categories } from './models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  categoryReg:AngularFirestoreCollection<Categories>
+  category:Observable<Categories[]>
 
-  constructor(private afs : AngularFirestore ) { }
+  constructor(private afs : AngularFirestore ) { 
+    this.categoryReg=this.afs.collection('categories',x=>x.orderBy('name','asc'));
+    this.category=this.categoryReg.snapshotChanges().pipe(map(category=>{
+     return category.map(c=>{
+       const data = c.payload.doc.data() as Categories;
+       data.id=c.payload.doc.id;
+       return data;
+     })
+   }))
+  }
 
   getCategories(){
-    return this.afs.collection('categories',x=>x.orderBy('name','asc')).snapshotChanges()
-      .pipe(map((course:any[])=>{
-        return course.map(c=>{
-          const data=c.payload.doc.data();
-          const id=c.payload.doc.id;
-          return <any>{id,...data}
-        })
-      }))
+   return this.category;
   }
 }
--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AddCourseService } from '../add-course.service';
 import { AddCourse } from '../models/add-course';
 import { switchMap } from 'rxjs/operators';
+import { CategoryService } from '../category.service';
+import { Categories } from '../models/category';
 
 @Component({
   selector: 'app-course-list',
@@ -10,15 +12,19 @@ import { switchMap } from 'rxjs/operators';
 })
 export class CourseListComponent implements OnInit {
   courses : AddCourse[];
+  category:Categories[];
 
-  constructor(private addCourseService : AddCourseService) { 
+  constructor(private addCourseService : AddCourseService,private catService : CategoryService) { 
      this.addCourseService.getCourses().subscribe(courses=>{
        this.courses=courses;
        console.log("Course",courses)
+       this.catService.getCategories().subscribe(category=>{
+         this.category=category;
+         console.log("Category",category)
+       })
      })
   }
 
   ngOnInit() {
   }
-
 }
